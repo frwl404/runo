ae1511a96551165c0e417bb1d35a6de9988d021f
Anton <anton.chivkunov@gmail.com>
2025-05-18 16:55:03 +0300

    Support update-deps for python36 container
    UV is used for managing dependencies in all containers, which supports UV,
    but in Python 3.6 UV is not supported and we should use pip-compile.
    Our script should support it.

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -2,4 +2,19 @@
 
 cd "$(dirname "$0")/../containers/$(printenv REGO_CONTAINER_NAME)/"
 
-uv lock --upgrade
+if command -v uv 2>&1 >/dev/null
+then
+    echo "updating deps with UV"
+    uv lock --upgrade
+    exit 0
+fi
+
+if command -v pip 2>&1 >/dev/null
+then
+    echo "updating deps with PIP"
+    pip-compile -U --cache-dir /tmp/.cache --strip-extras --output-file "pip.lock" "pip.in"
+    exit 0
+fi
+
+echo "Oops, can't find how to update dependencies :("
+
